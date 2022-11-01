d7f62ab5ffef847a16c1540d4706e4ce2854da6c
Calin Peters <calinpeters98@hotmail.nl>
2022-11-01 14:08:08 +0100

    Added custom authentication for Keycloak

---
+++ src/OcelotApiGateway/Authentication/ClaimsTransformer.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace OcelotApiGateway.Authentication
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            ClaimsIdentity claimsIdentity = (ClaimsIdentity)principal.Identity;
+
+            // flatten resource_access because Microsoft identity model doesn't support nested claims
+            // by map it to Microsoft identity model, because automatic JWT bearer token mapping already processed here
+            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "resource_access"))
+            {
+                var userRole = claimsIdentity.FindFirst((claim) => claim.Type == "resource_access");
+
+                var content = Newtonsoft.Json.Linq.JObject.Parse(userRole.Value);
+
+                foreach (var role in content["Kinoroom"]["roles"])
+                {
+                    claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()));
+                }
+            }
+
+            return Task.FromResult(principal);
+        }
+    }
+}
